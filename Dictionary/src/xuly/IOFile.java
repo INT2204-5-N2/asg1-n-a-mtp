65b0a2663431f7baef38f027541122b368380df0
Thế Tessen <thesondong@gmail.com>
2018-10-03 23:57:29 +0700

    demo

--- Dictionary/src/xuly/IOFile.java
+++ Dictionary/src/xuly/IOFile.java
@@ -1,38 +1,16 @@
 package xuly;
 
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-//package controller;
-
-
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.FileWriter;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.Map;
 import java.util.Scanner;
 import java.util.Set;
 import java.util.TreeMap;
 
 
 
-/**
- *
- * @author ThanTrieu@Pro
- */
-public class IOFile {
-    protected int count=0;
-
-    
+public class IOFile {  
     public void ghiTu(TreeMap <String,String> list){
         Set<String> keySet = list.keySet();
         try (PrintWriter pw = new PrintWriter(new File("data1.DAT"))){           
